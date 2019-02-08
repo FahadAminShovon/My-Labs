f2389497b4dfd531b28a7165e15db0115832c4fe
Fahad Amin <bm.amin.fahad@gamil.com>
2019-02-08 23:47:16 +0600

    bug fixed in domain checker

--- CSE420/Lab02/src/domainChecker.java
+++ CSE420/Lab02/src/domainChecker.java
@@ -54,6 +54,7 @@ public class domainChecker {
 	public static void checker() {
 		char arr[] = str.toCharArray();
 		for(int i=0;i<arr.length;i++) {
+			if(pState==invalid1)break;
 			char a = arr[i];
 			charChecker(a);
 			

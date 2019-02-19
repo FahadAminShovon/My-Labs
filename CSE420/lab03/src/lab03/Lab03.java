321399e12510d8875843374131c1eb6a8a4c8aba
Fahad Amin <bm.amin.fahad@gamil.com>
2019-02-19 16:17:22 +0600

    added lab 3

---
+++ CSE420/lab03/src/lab03/Lab03.java
@@ -0,0 +1,77 @@
+
+package lab03;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author 16101251
+ */
+public class Lab03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    	public static void readFromFile() {
+		BufferedReader br = null;
+		String line;
+		
+		try {
+			br = new BufferedReader(new FileReader("src\\lab03\\lab03.txt"));
+                        line = br.readLine();
+                        int x = Integer.parseInt(line);
+                        
+                        Pattern[] checkRegex = new Pattern[x];
+                        
+                        
+			for(int i=0;i<x;i++) {
+                            line = br.readLine();
+                            checkRegex[i]=Pattern.compile(line);
+                            //System.out.println(line);
+			}
+                        
+                        
+                        int y = Integer.parseInt(br.readLine());
+                        String [] str = new String[y];
+                        for(int i=0;i<y;i++) {
+                            line = br.readLine();
+                            str[i]=line;
+                            //System.out.println(line);
+			}
+                        
+                        for(int i=0;i<y;i++){
+                            String ss = str[i];
+                            
+                            boolean checker = true;
+                            
+                            for(int j=0;j<x;j++){
+                                if(checkRegex[j].matcher(ss).matches()){
+                                    System.out.println("Yes ,"+(j+1));
+                                    checker = false;
+                                    break;
+                                }
+                            }
+                            
+                            if(checker)System.out.println("No "+0);
+                            
+                        }
+                        
+                        
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		
+	}
+    
+    public static void main(String[] args) {
+        readFromFile();
+    }
+    
+}
